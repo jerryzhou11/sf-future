5114c02da676d3bc5ca9c710af1ca52428181030
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-02-24 01:03:15 -0800

    import visual assets

--- sf-future/src/App.jsx
+++ sf-future/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Landing from './Pages/Landing'
 import './App.css'
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
--- sf-future/src/Pages/Landing.jsx
+++ sf-future/src/Pages/Landing.jsx
@@ -1,13 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Landing(){
 
     return(
-    <div>
+    <div class = "flex flex-col items-center justify-center h-screen">
         <h1 className="text-3xl font-bold">
         Looking To The Horizon
         </h1>
-        <p className="text-lg">
+        <p className="text-lg mt-10">
         Clouds of worry, glimmers of hope in downtown San Francisco
         </p>
+        <Link to="">
+          <div className="mt-10 transition duration-300 w-48 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Start Reading
+          </div>
+        </Link>
       </div>
     )
 }
