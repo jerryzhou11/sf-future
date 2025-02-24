a6631beb8dd320320c1d5b93a90e8d9b8c5bff7f
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-02-24 00:43:52 -0800

    vite setup + init landing page

---
+++ sf-future/src/Pages/Landing.jsx
@@ -0,0 +1,15 @@
+function Landing(){
+
+    return(
+    <div>
+        <h1 className="text-3xl font-bold">
+        Looking To The Horizon
+        </h1>
+        <p className="text-lg">
+        Clouds of worry, glimmers of hope in downtown San Francisco
+        </p>
+      </div>
+    )
+}
+
+export default Landing;
