cc30f0ee2b70ef4b78d0cf21b6af53fdbb0ff204
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-03-10 03:00:32 -0700

    the fix didn't work???

--- sf-future/src/Components/Scrolly.jsx
+++ sf-future/src/Components/Scrolly.jsx
@@ -16,7 +16,7 @@ function Scrolly({ children, backgroundImage, hideImageOnMobile = false}) {
               </div>
           </figure>}
           <div className="relative">
-            <div className="relative z-10 max-w-2xl md:max-w-4xl mx-auto px-4 opacity-[99] [transform:translate3d(0,0,0)] [-webkit-transform:translate3d(0,0,0)]">
+            <div className="relative z-10 max-w-2xl md:max-w-4xl mx-auto px-4 opacity-[99] transform-gpu [-webkit-transform:translate3d(0,0,0)]">
                 {children}
             </div>
           </div>
@@ -35,7 +35,7 @@ function Scrolly({ children, backgroundImage, hideImageOnMobile = false}) {
           </div>
       </figure>}
       <div className="relative">
-        <div className="relative z-20 max-w-2xl md:max-w-4xl mx-auto px-4 opacity-[99] [transform:translate3d(0,0,0)] [-webkit-transform:translate3d(0,0,0)]">
+        <div className="relative z-20 max-w-2xl md:max-w-4xl mx-auto px-4 opacity-[99] transform-gpu [-webkit-transform:translate3d(0,0,0)]">
             {children}
         </div>
       </div>
