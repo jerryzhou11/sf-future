12d0aa782b167f32b6eadf81f7a7a6e8e7d6a550
Jerry Zhou <122940736+kdfchse@users.noreply.github.com>
2025-03-10 00:53:50 -0700

    fix tab title and icon

--- sf-future/src/App.jsx
+++ sf-future/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Landing from './Pages/Landing'
 import Story from './Pages/Story'
 import './App.css'
